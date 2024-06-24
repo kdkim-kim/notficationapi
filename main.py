6010e588665428f9850a83905346969850ed5052
kdkim-kim <warriorkim68@gmail.com>
2024-06-24 18:02:18 +0900

    1 test

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ fake_user_db = { # 가상 유저 DB
 }
 
 class Token(BaseModel): # 프론트엔드 유저의 토큰 값
-    access_token: str
-    token_type: str
+    access_token: str # 보안토큰
+    token_type: str # 토큰 타입
 
-class TokenData(BaseModel): # 토큰의내용
+class TokenData(BaseModel): # 토큰이 가지고 있는 정보
     username : str | None = None
 
 class User(BaseModel): #
@@ -41,7 +41,7 @@ class User(BaseModel): #
     full_name : Union[str, None] = None
     disabled : Union[bool, None] = None
 
-class UserInDB(User): # 해시 패스워드 값
+class UserInDB(User): # User 클래스 상속 하고 해시패스워드를 추가함
     hashed_password : str
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") #
@@ -50,8 +50,10 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token") # 보안 토큰
 
 app = FastAPI()
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password) # 패스워드 검증
+def verify_password(plain_password, hashed_password): # 입력 패스워드와 디비에 해시된 패스워드 값 비교
+    result = pwd_context.verify(plain_password, hashed_password)
+    print(result, plain_password, hashed_password)
+    return pwd_context.verify(plain_password, hashed_password) # 참 / 거짓 반환
 
 def get_password_hash(password):
     return pwd_context.hash(password) # 패스워드 해시값 반환
@@ -62,36 +64,39 @@ def fake_hash_password(password: str):
 
 def get_user(db, username:str): # 유저가 DB 에 있으면 해시패스워드 반환
     if username in db: # 사용자명이 디비에 있으면
-        user_dict = db[username]
-        return UserInDB(**user_dict)
+        user_dict = db[username] # 사용자 정보
+        print(user_dict)
+        return UserInDB(**user_dict) # 해시된 패스워드 정보를 포함 한 값 반환
 
-def authenticate_user(fake_db, username: str, password: str): # 입력데이타와 디비의 유져 자료 비교 검증
-    user = get_user(fake_db, username)
-    if not user:
+def authenticate_user(fake_db, username: str, password: str): # 앤드프런트 입력데이타와 디비의 유져 자료 비교 검증
+    user = get_user(fake_db, username) # 사용자명을 기준으로 데이타 조회 후 사용자의 해시된 패스워드 값을 포함함 노즌 정보 user에 할당
+    if not user: # 사용자 명이 없으면
         return False
-    if not verify_password(password, user.hashed_password):
+    if not verify_password(password, user.hashed_password): # 프런트앤드 입력 패스워드와 디비의 해시된 패스워드와 비교
         return False
     return user
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict, expires_delta: timedelta | None = None): # 토큰 생성
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
+    if expires_delta: # 로그인한 경우 (최초)
+        expire = datetime.now(timezone.utc) + expires_delta # 현재에서 정한시간만큼 더함
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15) # 이미 로그인해서 사용 중인 경우 현재에서 15분 더함
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    print(encoded_jwt)
     return encoded_jwt
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]): # 현재의 보안 토큰에서
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]) # 토큰이 가지고 있는 정보 값을 가져옴
+        print(payload)
+        username: str = payload.get("sub") #사용자 이름 값
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
@@ -108,16 +113,19 @@ async def get_current_active_user(current_user: Annotated[User, Depends(get_curr
     return current_user
 
 @app.post("/token")
-async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token: # 폼 데이타 토큰으로
-    user  = authenticate_user(fake_user_db, form_data.username, form_data.password)
-    if not user:
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token: # 프론트 앤드 사용자 입력폼 데이타 토큰클래스로
+    user  = authenticate_user(fake_user_db, form_data.username, form_data.password) # 사용자를 자료와 비교 검증하고 user[list]에 사용자 정보를 할당
+    print(user)
+    if not user: # 로그인 정보가 맞지 않을시
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES) # 토큰 유효시간
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires) # 토큰 생성
+    result = Token(access_token=access_token, token_type="bearer")
+    print(result)
     return Token(access_token=access_token, token_type = "bearer")
 
 @app.get("/users/me/", response_model=User)
